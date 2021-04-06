592d90e84340d3808c813b2275db8d3bb0cfe016
Valērijs Stundiņš <vstund@gmail.com>
2021-04-06 19:22:03 +0300

    Second homework

---
+++ src/lv/lu/homework1/MyFirstHomeWork.java
@@ -0,0 +1,7 @@
+package lv.lu.homework1;
+
+public class MyFirstHomeWork {
+    public static void main(String[] args) {
+        System.out.println("My dghfhjfgfhfgfhghf");
+    }
+}
---
+++ src/lv/lu/homework2/MySecondHomeWork.java
@@ -0,0 +1,7 @@
+package lv.lu.homework2;
+
+public class MySecondHomeWork {
+    public static void main(String[] args) {
+        System.out.println("22222");
+    }
+}
