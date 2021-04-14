1fd7144945422749e2695c6ab27d7d32e39d653a
Valērijs Stundiņš <vstund@gmail.com>
2021-04-14 21:04:10 +0300

    copy from teacher

---
+++ app/src/main/lv.lu.finalwork/repository/Repository.java
@@ -0,0 +1,14 @@
+package teacher.finalwork.repository;
+
+import java.util.List;
+
+public interface Repository<T> {
+
+    Long save(T entity);
+
+    List<T> findAll();
+
+    T findById(Long id);
+
+    void delete(Long id);
+}
